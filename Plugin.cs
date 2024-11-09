b70ac014cd64503dc3300df6c5e129601c92198d
Zeffies <78884575+Zeffies@users.noreply.github.com>
2024-11-08 23:04:58 -0500

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace EnableDebugPlugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        var harmony = new Harmony("com.Zeffies.EnableDebugPlugin");
+        harmony.PatchAll();
+
+    }
+}
+[HarmonyPatch(typeof(AnNGUIOptionMenu), "CreateNewCurrentMenuItem")]
+public class AnNGUIOptionMenu_CreateNewCurrentMenuItem
+{
+    [HarmonyPrefix]
+    static void SetDebugMenuOn()
+    {
+        AnSingleton<AnGameMgr>.Instance.GetDbgCheatData().m_isUseDbgMenuInGame = true;
+    }
+}
